c09fc9f5371bcb5ed50a7112b6aea74fed2483c9
nikhil18 <nikhilgolechha1@gmail.com>
2017-07-30 02:01:35 -0700

    tune smartmax a little

--- drivers/cpufreq/cpufreq_smartmax_big.c
+++ drivers/cpufreq/cpufreq_smartmax_big.c
@@ -46,7 +46,7 @@
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
 
-#define DEFAULT_SUSPEND_IDEAL_FREQ 1056000
+#define DEFAULT_SUSPEND_IDEAL_FREQ 576000
 #define DEFAULT_AWAKE_IDEAL_FREQ 576000
 #define DEFAULT_RAMP_UP_STEP 200000
 #define DEFAULT_RAMP_DOWN_STEP 200000
