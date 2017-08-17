d7592ac26f170c55e9e76e96c0fdda2b9bec3cd2
nikhil18 <nikhilgolechha1@gmail.com>
2017-08-17 13:05:29 -0700

    bump to version 9

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ KERNEL="Image.gz-dtb"
 DEFCONFIG="lightning_defconfig"
 
 # Kernel Details
-VER=Lightning-Kernel-V8
-VARIANT="OP5-OOS-N-V8"
+VER=Lightning-Kernel-V9
+VARIANT="OP5-OOS-N-V9"
 
 # Vars
 export LOCALVERSION=~`echo $VER`
