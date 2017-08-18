71b2268d84f702da8f7fda2fa9f06ceb0710dbea
nikhil18 <nikhilgolechha1@gmail.com>
2017-08-18 00:56:05 -0700

    Revert "target max frequency"
    This reverts commit 458abb4470649fbf35334531ebc1d19497c51944.

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -78,8 +78,8 @@ struct cpufreq_suspend_t {
 };
 
 static DEFINE_PER_CPU(struct cpufreq_suspend_t, suspend_data);
-#define LITTLE_CPU_QOS_FREQ 2035200
-#define BIG_CPU_QOS_FREQ    2592000
+#define LITTLE_CPU_QOS_FREQ 1900800
+#define BIG_CPU_QOS_FREQ    2361600
 
 unsigned int cluster1_first_cpu = 0;
 static bool qos_cpufreq_flag = false;
