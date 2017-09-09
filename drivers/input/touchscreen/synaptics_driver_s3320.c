ebf75bab9a459beae5b38089f75947fa582defd9
nikhil18 <nikhilgolechha1@gmail.com>
2017-09-09 08:36:20 -0700

    Revert "Synchronize codes for OxygenOS 4.5.10"
    This reverts commit 592975c9a5c920e7e680f0f20d5191896fe67289.

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -2156,7 +2156,7 @@ static void checkCMD(void)
 	int flag_err = 0;
 	struct synaptics_ts_data *ts = ts_g;
 	do {
-		delay_qt_ms(30); /*wait 30ms*/
+		delay_qt_ms(10); //wait 10ms
 		ret = synaptics_rmi4_i2c_read_byte(ts->client, F54_ANALOG_COMMAND_BASE);
 		flag_err++;
 	}while( (ret > 0x00) && (flag_err < 30) );
